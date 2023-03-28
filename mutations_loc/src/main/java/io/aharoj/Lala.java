689ff3923adee01e480e5f782b3e0dd60a515b2f
Angel J. Haro <haro.j.angel@gmail.com>
2023-03-27 17:21:20 -0700

    loc

---
+++ mutations_loc/src/main/java/io/aharoj/Lala.java
@@ -0,0 +1,16 @@
+package io.aharoj;
+
+public class Lala 
+{
+    public boolean isPositive(int number) 
+    {
+
+        boolean result = false;
+        if (number >= 0) 
+        {
+            result = true;
+        }
+        return result;
+        
+    }
+}
