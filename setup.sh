79eb5867de9df1afb00d0c0cf4a106e4032142dc
Angel J. Haro <haro.j.angel@gmail.com>
2024-04-21 23:26:30 -0700

    prompt feature

--- setup.sh
+++ setup.sh
@@ -2,31 +2,31 @@
 
 # Function to print a banner message
 print_banner() {
-    echo "--------------------------------------------------------------------------------"
-    echo ""
-    echo "$1"
-    echo ""
-    echo "--------------------------------------------------------------------------------"
+	echo "--------------------------------------------------------------------------------"
+	echo ""
+	echo "$1"
+	echo ""
+	echo "--------------------------------------------------------------------------------"
 }
 
 # Function to check Java version
 check_java_version() {
-    local version=$(java -version 2>&1 | awk -F '"' '/version/ {print $2}' | awk -F '.' '{print $1}')
-    echo "Java major version: $version"
-    if [[ "$version" -ne "17" ]]; then
-        echo "Java version is not 17. The script requires Java 17 to run correctly."
-        exit 1
-    fi
+	local version=$(java -version 2>&1 | awk -F '"' '/version/ {print $2}' | awk -F '.' '{print $1}')
+	echo "Java major version: $version"
+	if [[ "$version" -ne "17" ]]; then
+		echo "Java version is not 17. The script requires Java 17 to run correctly."
+		exit 1
+	fi
 }
 
 # Determine OS and validate
 if [ "$(uname)" == "Darwin" ]; then
-    echo "Running on MacOS."
+	echo "Running on MacOS."
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    echo "Running on Linux."
+	echo "Running on Linux."
 else
-    echo "Unsupported operating system. This script only supports MacOS and Linux."
-    exit 1
+	echo "Unsupported operating system. This script only supports MacOS and Linux."
+	exit 1
 fi
 
 # Step 0: Check Java version
@@ -34,19 +34,19 @@ check_java_version
 
 # Step 1: Run the Maven commands if the POM file exists
 if [ -f pom.xml ]; then
-    mvn clean package compile
-    print_banner "Maven commands executed"
+	mvn clean install -DskipTests
+	print_banner "Maven build and installation completed without running tests"
 else
-    print_banner "No pom.xml file found. Make sure you're in the right directory and that the project is a Maven project"
+	print_banner "No pom.xml file found. Make sure you're in the right directory and that the project is a Maven project."
 fi
 
 # Step 2: Check if pip3 is installed
 if command -v pip3 &>/dev/null; then
-    print_banner "pip3 is installed"
+	print_banner "pip3 is installed"
 else
-    print_banner "pip3 is not installed, installing now"
-    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-    python3 get-pip.py
+	print_banner "pip3 is not installed, installing now"
+	curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+	python3 get-pip.py
 fi
 
 # Step 3: Install littledarwin
@@ -55,8 +55,8 @@ print_banner "littledarwin installed"
 
 # Step 4: Run the LittleDarwin command if the source directory exists
 if [ -d "./src/main" ]; then
-    python3 -m littledarwin -m -b -t ./ -p ./src/main
-    print_banner "LittleDarwin command executed"
+	python3 -m littledarwin -m -b -t ./ -p ./src/main
+	print_banner "LittleDarwin mutation testing executed"
 else
-    print_banner "The source directory ./src/main does not exist. Please check your directory structure."
+	print_banner "The source directory ./src/main does not exist. Please check your directory structure."
 fi
