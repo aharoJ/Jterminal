7f81ebb1b017fca7f4c24464b15c4b91ebd1edf2
Angel J. Haro <haro.j.angel@gmail.com>
2025-03-04 19:04:49 -0800

    app

--- jterminal/src/main/java/io/aharoj/App.java
+++ jterminal/src/main/java/io/aharoj/App.java
@@ -5,10 +5,8 @@ package io.aharoj;
  * @author for mutation research Angel J. Haro
  * @see https://aharoj.io/
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
 }
